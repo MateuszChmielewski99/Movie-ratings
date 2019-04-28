ac498c706c95ab7ff8a7cb1e6a4c8d6eeaa90895
Mateusz Chmielewski <chmielewski079@gmail.com>
2019-04-28 15:43:30 +0200

    added register button

--- Movie-recommendation/Movie-recommendation/MainWindow.xaml.cs
+++ Movie-recommendation/Movie-recommendation/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Movie_recommendation
 {
@@ -46,14 +36,14 @@ namespace Movie_recommendation
         private void BtnLog_Click(object sender, RoutedEventArgs e)
         {
             Loading load = new Loading();
-
-            Task t = Task.Run(async () => {
+            Task t = Task.Run(async () =>
+            {
                 string name = TBUserName.Dispatcher.Invoke(() => TBUserName.Text);
                 string password = PBPassword.Dispatcher.Invoke(() => PBPassword.Password.ToString());
                 string tmp = await load.LoadUser(name, password);
                 LblInfo.Dispatcher.Invoke(() => LblInfo.Content = tmp);
-                }
-            ); 
+            }
+            );
         }
 
     }
